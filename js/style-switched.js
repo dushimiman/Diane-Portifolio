2ac427acdd461c9ef84d31a2833729dd1f08a661
Dushimimana Diane <dushimediane12@gmail.com>
2022-05-19 21:15:23 +0200

    add javascript

---
+++ js/style-switched.js
@@ -0,0 +1,32 @@
+//toggle style-switcher
+// const styleSwitcherToggle = document.querySelector("style-switcher-toggler");
+
+// styleSwitcherToggle.addEventListener("click" , () =>{
+//     document.querySelector(".style-switcher").classList.toggle("open");
+// })
+//hide style switcher on scroll
+// window.addEventListener("scroll", () =>{
+//     if(document.querySelector(".style-switcher").classList.contains("open"))
+//     {
+//         document.querySelector(".style-switcher").classList.remove("open");
+//     }
+// })
+
+const alternateStyle = document.querySelectorAll(".alternate-style");
+
+function setActiveStyle(color)
+{
+    localStorage.setItem("color", color);
+    changeColor();
+}
+function changeColor(){
+    alternateStyle.forEach((style) => {
+        if(localStorage.getItem("color") === style.getAttribute("title"))
+        {
+style.removeAttribute("disabled");
+        }
+        else{
+            style.setAttribute("disabled", "true");  
+        }
+    })
+}
